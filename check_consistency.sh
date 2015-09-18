dd3509238da24187f763dc15a2b5b7b0c77f4685
Nils <area51.nils@googlemail.com>
2015-09-18 15:37:36 +0200

    More lines in script

--- check_consistency.sh
+++ check_consistency.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-grep --include="*.tex" "\\ref{[^}]*}" -r -o | sort
-grep --include="*.tex" "\\label{[^}]*}" -r -o | sort
+# grep --include="*.tex" "\\ref{[^}]*}" -r -o | sort
+# grep --include="*.tex" "\\label{[^}]*}" -r -o | sort
+
+
+#grep --include="*.tex" "(?<=cite{)[^}]*(?=})" -r -o -h -P | sort -u
+
+grep --include="*.tex" "(?<=label{)[^}]*(?=})" -r -o -h -P | uniq -d
